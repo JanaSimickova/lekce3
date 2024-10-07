68603d476dbe3f92e0743efdecd4249e72c6bc88
Jana Šimíčková <jana.oravska@gmail.com>
2024-10-07 18:58:20 +0200

    Vyresene priklady na vstup a vystup

---
+++ script.js
@@ -0,0 +1,14 @@
+// Cviceni 1 - Ockovani
+// const jmeno = prompt("Zadej své celé jméno");
+// const vek = Number(prompt(("Zadej svůj věk")));
+
+// document.body.innerHTML += "<p>" + jmeno + ", věk:" + vek + "</p>";
+// alert(jmeno + ", vek: " + vek);
+
+// Cviceni 2 - Vyplata jako stranka
+
+const hodinovaSazba = Number(prompt("Zadej výši tvé hodinové sazby:"));
+const pocetHodin = Number(prompt("Zadej, kolik hodin denně pracuješ:"));
+const pocetDni = Number(prompt("Zadej, kolik dní v měsíci pracuješ:"));
+
+document.body.innerHTML += "<p> Vyděláváš " + hodinovaSazba * pocetHodin * pocetDni + ",- Kč měsíčně.</p>";
